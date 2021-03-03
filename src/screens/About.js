b929927eefa139eaa1b195fd656adab655ffde5f
John Lovato <jtlovato@gmail.com>
2021-03-03 12:27:18 -0700

    Finished contact section

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,29 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
-    // return (
-    //     <div>
-    //         about
-    //     </div>
-    // )
+
+    var one_day = 1000 * 60 * 60 * 24 
+    var present_date = new Date(); 
+    var creation = new Date(1986, 5, 19) 
+    var Result = Math.round( present_date.getTime() - creation.getTime()) / (one_day); 
+    var Final_Result = Result.toFixed(0); 
+    let birthday = (Final_Result); 
+    
     return (
-        <div className="homepage">
-            <div className="card-around">
-                <div className="card-container">
-                    <div className="opaque">
-                        <img src="/img/about.png" className="opaque-text"></img>
+        <div className="about-page">
+            <div className="header">
+                <h1>about me?</h1>
+            </div>
+            <div className="about-container">
+                    <img
+                        className="about-pic"
+                        src="../img/me.png"
+                    />
+                <div className="about-info-container">
+                    <div className="about-info">
+                        <h1>I Never Know What To Put Here...</h1>
+                        <p>{birthday} days ago, <span>John T. Lovato</span> was created. This had made many people very angry and has been widely regarded as a bad move. In the time since, John has spent his time studying <span>designs</span>, ranging from television ads and internet social medias, to print splashes and billboards, refining what looks good. Recently, he graduated from Treehouse Online Bootcamp for <span>front-end web development</span>. Once he has a focus on his project he doesn't let up until it's complete and looks amazing, utilizing an almost honey badger-esque mindframe.</p>
+                        <p><q>Passion is Energy</q></p>
+                        <p>John has lived in several parts of the US and currently calls <span>Denver</span> his home. When he's not on VCS or Photoshop, John likes to play video games, watches and plays hockey, roots for the Avalanche, and plays with his <span>two dogs</span>.</p>
                     </div>
                 </div>
             </div>
-
-            <div className="card-around">
-                <div className="card-container">
-                    <div className="opaque">
-                        <img src="/img/projects.png" className="opaque-text"></img>
-                    </div>
-                </div>
-            </div>
-
-</div>
+        </div>
     )
 }
--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -2,38 +2,34 @@ import React from 'react'
 
 export default function Contact() {
     return (
-        <div className="homepage">
-            <div className="card-around">
-                <div className="card-container">
-                    <div className="opaque">
-                        <img src="/img/about.png" className="opaque-text"></img>
-                    </div>
-                </div>
+        <div className="contact-container">
+            <div className="header">
+                <h1>contact me</h1>
             </div>
-
-            <div className="card-around">
-                <div className="card-container">
-                    <div className="opaque">
-                        <img src="/img/projects.png" className="opaque-text"></img>
-                    </div>
-                </div>
-            </div>
-
-            <div className="card-around">
-                <div className="card-container">
-                    <div className="opaque">
-                        <img src="/img/projects.png" className="opaque-text"></img>
-                    </div>
-                </div>
-            </div>
-            <div className="card-around">
-                <div className="card-container">
-                    <div className="opaque">
-                        <img src="/img/projects.png" className="opaque-text"></img>
-                    </div>
-                </div>
-            </div>
-
-</div>
+            <a href="mailto:johnsawebdev@gmail.com" className="link-container">
+                <img src="./img/email.jpg" className="color" />
+                <img src="./img/email-grey.jpg" className="grey" />
+            </a>
+            <a href="tel:682-283-1972" className="link-container">
+                <img src="./img/call.jpg" className="color" />
+                <img src="./img/call-grey.jpg" className="grey" />
+            </a>
+            <a className="link-container">
+                <img src="./img/address.jpg" className="color" />
+                <img src="./img/address-grey.jpg" className="grey" />
+            </a>
+            <a href="https://www.linkedin.com/in/john-lovato-4295b3199/" className="link-container">
+                <img src="./img/linkedin.jpg" className="color" />
+                <img src="./img/linkedin-grey.jpg" className="grey" />
+            </a>
+            <a href="https://twitter.com/JohnTLovato" className="link-container">
+                <img src="./img/twitter.jpg" className="color" />
+                <img src="./img/twitter-grey.jpg" className="grey" />
+            </a>
+            <a href="https://github.com/JTLovato/" className="link-container">
+                <img src="./img/github.jpg" className="color" />
+                <img src="./img/github-grey.jpg" className="grey" />
+            </a>
+        </div>
     )
 }
